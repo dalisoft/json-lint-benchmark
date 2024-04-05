560e819553e669957251e822dbcc5ec6a0223c74
Davlatjon Shavkatov <opensource@dalisoft.uz>
2024-04-05 05:39:09 +0500

    fix: improve benchmarking

--- benchmark.sh
+++ benchmark.sh
@@ -6,8 +6,15 @@ export PATH="./node_modules/.bin:$PATH"
 
 json_files="package-template package-lock-test twitter"
 
+prepare() {
+  /bin/rm -rf "$HOME/Library/Caches/deno/fmt_*"
+}
+
+# shellcheck disable=SC3045
+export -f prepare
+
 # shellcheck disable=SC2034
 for item in ${json_files}; do
   file="data/$item.json"
-  hyperfine --runs 3 "biome format $file" "prettier -c $file" "dprint fmt --check $file" "jsona fmt --option trailing_newline=true --check $file" "spectral lint --ignore-unknown-format $file" --export-markdown "results/$item.md" --ignore-failure
+  hyperfine --runs 3 "biome format $file" "prettier -c $file" "dprint fmt --incremental=false $file" "deno fmt $file" "jsona fmt --option trailing_newline=true --check $file" "spectral lint --ignore-unknown-format $file" --export-markdown "results/$item.md" --prepare prepare
 done
