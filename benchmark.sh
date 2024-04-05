641f6c796318f27424743dd32f5de44e68b9902c
Davlatjon Shavkatov <opensource@dalisoft.uz>
2024-04-05 05:20:32 +0500

    fix(benchmark): improve `dprint` results and update `warmup`

--- benchmark.sh
+++ benchmark.sh
@@ -9,5 +9,5 @@ json_files="package-template package-lock-test twitter"
 # shellcheck disable=SC2034
 for item in ${json_files}; do
   file="data/$item.json"
-  hyperfine --runs 10 --warmup 3 "biome check $file" "prettier -c $file" "dprint check $file" "jsona fmt --option trailing_newline=true --check $file" "spectral lint --ignore-unknown-format $file" --export-markdown "results/$item.md" --ignore-failure
+  hyperfine --runs 3 "biome check $file" "prettier -c $file" "dprint fmt --check $file" "jsona fmt --option trailing_newline=true --check $file" "spectral lint --ignore-unknown-format $file" --export-markdown "results/$item.md" --ignore-failure
 done
